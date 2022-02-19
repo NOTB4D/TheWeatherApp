0c3367b7cfe2a1bf2d7bf3243b044801f4be7360
Eser kucuker <eserkucuker@gmail.com>
2022-02-19 20:20:26 +0300

    Refactor ClientNetworService

--- TheWeatherApp/TheWeatherApp/ClinetNetworkService.swift
+++ TheWeatherApp/TheWeatherApp/ClinetNetworkService.swift
@@ -1,5 +1,5 @@
 //
-//  ClientService.swift
+//  ClinetNetworkService.swift
 //  TheWeatherApp
 //
 //  Created by Eser Kucuker on 19.02.2022.
@@ -8,14 +8,21 @@
 import Foundation
 
 
-class ClinetService {
+class ClinetNetworkService {
+    
+    enum Endpoints: String{
+        case currentWeather = "weather?lat=%d&lon=%d"
+        
+        func urlString(arguments: CVarArg...)-> String {
+            return String(format: self.rawValue, arguments)
+        }
+    }
     
    private let networkService: NetworkServiceProtocol = NetworkService()
     
     func getCurrentWeather(with latitude:Double, longitude: Double, complation: @escaping(CurrentWeatherDTO?, NetworkError?)-> Void){
         
-        let urlString = String(format: "weather?lat=%d&lon=%d", latitude, longitude)
-        
+        let urlString = Endpoints.currentWeather.urlString(arguments: latitude, longitude)
         networkService.get(from: urlString, completion: complation)
     }
 }
