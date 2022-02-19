92452f8d5754c262ab37a5e48dd049b7a2135f18
Eser kucuker <eserkucuker@gmail.com>
2022-02-19 19:16:02 +0300

    Create project

---
+++ TheWeatherApp/TheWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheWeatherApp
+//
+//  Created by Eser Kucuker on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
