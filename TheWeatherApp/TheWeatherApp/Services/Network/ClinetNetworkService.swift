25f7997adfd3fa83bcdd14909cda9a85af371395
Eser kucuker <eserkucuker@gmail.com>
2022-02-19 23:56:00 +0300

    Add updateLabel func

--- TheWeatherApp/TheWeatherApp/Services/Network/ClinetNetworkService.swift
+++ TheWeatherApp/TheWeatherApp/Services/Network/ClinetNetworkService.swift
@@ -13,20 +13,20 @@ class ClinetNetworkService: ClientNetworkServiceProtocol {
    private enum Endpoints: String{
         case currentWeather = "weather?lat=%f&lon=%f"
         //TODO: Buraya Bak
-      func urlString(arguments: Double...)-> String {
+      /*func urlString(arguments: Double... )-> String {
             return String(format: self.rawValue, arguments)
-        }
-     /* func urlString(latitude: Double, longitude:Double)-> String {
+        }*/
+    func urlString(latitude: Double, longitude:Double)-> String {
              return String(format: self.rawValue, latitude,longitude)
-         }*/
+         }
     }
     
    private let networkService: NetworkServiceProtocol = NetworkService()
     
     func getCurrentWeather(latitude:Double, longitude: Double, complation: @escaping(CurrentWeatherDTO?, NetworkError?)-> Void){
         
-        let urlString = Endpoints.currentWeather.urlString(arguments: latitude, longitude )
-        //let urlString = Endpoints.currentWeather.urlString(latitude: latitude, longitude:longitude )
+        //let urlString = Endpoints.currentWeather.urlString(arguments: latitude, longitude )
+        let urlString = Endpoints.currentWeather.urlString(latitude: latitude, longitude:longitude )
         networkService.get(from: urlString, completion: complation)
     }
 }
